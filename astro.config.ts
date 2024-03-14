3ed1f21b340fc3cc45aa8947edd673e12810e537
Williams Ogunjide <121948920+William-Ademola@users.noreply.github.com>
2024-03-14 12:56:42 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import unocss from "unocss/astro";
 import vesper from "./src/lib/vesper.json";
 
 export default defineConfig({
-  site: "https://www.nexxel.dev",
+  site: "https://www.williamsindepth.dev",
   adapter: vercel(),
   output: "hybrid",
   integrations: [
